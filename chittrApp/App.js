6bd6d23386d1f4f3121924f94cbb07632e8ae7d1
Aaron <57218797+masseyTM@users.noreply.github.com>
2020-03-10 10:49:05 +0000

    Update App.js
    Style updates

--- chittrApp/App.js
+++ chittrApp/App.js
@@ -7,59 +7,80 @@
  */
 
 import React, { Component } from 'react';
-import { FlatList, ActivityIndicator, Text, View } from 'react-native';
+import { FlatList,StyleSheet, ActivityIndicator, Text, View } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#E8E8E8',
+    flex: 10,
+    display: 'flex',
+
+  },
+  titleText: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    alignItems: 'center',
+    marginLeft: 15,
+    marginTop: 10,
+    color: 'white',
+  },
+  chitsText: {
+    fontSize: 20,
+    marginTop: 10,
+  }
+  });
 
 class Lab03ex3 extends Component {
- constructor(props){
- super(props);
- this.state ={
-isLoading: true,
-shoppingListData: []
-}
- }
+  constructor(props){
+  super(props);
+  this.state ={
+    isLoading: true,
+    allChits: []
+    }
+  }
 
   getData(){
- return fetch('http://10.0.2.2:3333/api/v0.0.5/chits')
- .then((response) => response.json())
- .then((responseJson) => {
- this.setState({
- isLoading: false,
- shoppingListData: responseJson,
- });
- })
- .catch((error) =>{
- console.log(error);
- });
- }
+    return fetch('http://10.0.2.2:3333/api/v0.0.5/chits')
+    .then((response) => response.json())
+    .then((responseJson) => {
+        this.setState({
+          isLoading: false,
+          allChits: responseJson,
+          });
+    })
+    .catch((error) =>{
+      console.log(error);
+      });
+    }
 
  componentDidMount(){
  this.getData();
 }
 
  render() {
-
- if(this.state.isLoading){
- return(
- <View>
- <ActivityIndicator/>
- </View>
- )
- }
- return (
-<View>
- <FlatList
- data={this.state.shoppingListData}
- renderItem={({item}) => <Text>{item.chit_id} "/n" {item.chit_content}</Text>}
- keyExtractor={({id}, index) => id}
- />
- </View>
-
-
- );
- }
-
-
-}
+   if(this.state.isLoading){
+     return(
+       <View>
+       <ActivityIndicator/>
+       </View>
+       )
+     }
+     return (
+       <React.Fragment>
+         <View style={{ flex: 1, backgroundColor: '#1D9DDB'}}>
+            <Text style={styles.titleText}>Chittr</Text>
+         </View>
+         <View style={styles.container}>
+          <FlatList
+            data={this.state.allChits}
+            renderItem={({item}) => <Text style={styles.chitsText}>{item.user.given_name} {"\n"}{item.chit_content}</Text>}
+            keyExtractor={({id}, index) => id}
+            />
+         </View>
+       </React.Fragment>
+       );
+     }
+   }
 
 
 
